19a9a25aeade052ce02abf76f73978409e56ecac
Rachen Tanimkarn <onemanking@hotmail.com>
2018-09-06 16:25:11 +0700

    added cube clicked & main menu loading animation

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -2,18 +2,29 @@
 using System.Collections;
 using UnityEngine.EventSystems;
 
-public class Cube : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler {
+public class Cube : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
+{
 
     [SerializeField] private TextMesh cubeText;
     public int cubeNumber;
     private GridManager gridManager;
+    private Animator anim;
+    private bool isHover;
 
     // Use this for initialization
     void Start()
     {
         gridManager = GameObject.FindWithTag("Grid").GetComponent<GridManager>();
-
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
         SetNumber(gridManager.RandomNumber());
+    }
+
+    void Update() {
+        if (isHover)
+        {
+            transform.localScale = new Vector3(Mathf.Lerp(transform.localScale.x, 1.1f, Time.deltaTime*10), Mathf.Lerp(transform.localScale.y, 1.1f, Time.deltaTime*10), 1);
+        }
     }
 
     void SetNumber(int number)
@@ -44,7 +55,10 @@ public class Cube : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IP
 
         if (cubeNumber == gridManager.currentNumber)
         {
-            Destroy(gameObject);
+            isHover = false;
+            
+            StartCoroutine(PlayClickAndWait());
+            
             gridManager.currentNumber += 1;
             if (gridManager.cubeInGrid < gridManager.maxNumber)
             {
@@ -64,11 +78,37 @@ public class Cube : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IP
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        isHover = true;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-       
+        isHover = false;
+        transform.localScale = Vector2.one;
     }
+
+    void PlayAnimation(string animationString)
+    {
+        anim.SetBool(animationString, true);
+    }
+
+    IEnumerator PlayClickAndWait()
+    {
+        anim.enabled = true;
+        anim.SetTrigger("clicked");
+
+        cubeText.GetComponent<Renderer>().sortingOrder = 1;
+
+        while (anim.GetCurrentAnimatorStateInfo(0).length > anim.GetCurrentAnimatorStateInfo(0).normalizedTime)
+        {
+            yield return null;
+        }
+
+        cubeText.GetComponent<Renderer>().sortingOrder = -10;
+
+        yield return new WaitForEndOfFrame();
+
+        Destroy(gameObject);
+    }
+
 }
